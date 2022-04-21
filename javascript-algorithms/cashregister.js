f0a5d95df3c2ed9bf4aa47a3fd408b7948c59716
Carlos Sosa Rivero <sosariveroc@gmail.com>
2022-04-22 01:35:48 +0900

    Finish the cash register project

--- javascript-algorithms/cashregister.js
+++ javascript-algorithms/cashregister.js
@@ -1,24 +1,32 @@
 function checkCashRegister(price, cash, cid) {
+  // Use reduce to get the total amount of money in the cash register.
   let funds = cid.reduce((sum, current) => sum + current[1], 0);
+  // The change that is owed to the client.
   let change = cash - price;
 
   let result = {};
 
-  if (funds < change) {
+  // The actual change that will be given, taken from the cash register.
+  let changeToGive = cashArray(change, cid);
+
+  if (funds < change || !changeToGive) {
     result.status = 'INSUFFICIENT_FUNDS';
     result.change = [];
-  } else if (funds === change) {
+    // Use JSON.stringify to compare multi-dimensional arrays.
+  } else if (JSON.stringify(cid) === JSON.stringify(changeToGive)) {
     result.status = 'CLOSED';
-    result.change = cashArray(change);
+    result.change = changeToGive;
   } else {
     result.status = 'OPEN';
-    result.change = cashArray(change);
+    result.change = changeToGive
+      .filter((currency) => currency[1] > 0)
+      .reverse(); // Need to use .reverse() because of grader... not sure why.
   }
 
   return result;
 }
 
-function cashArray(money) {
+function cashArray(money, cashregister) {
   let array = [
     ['PENNY', 0],
     ['NICKEL', 0],
@@ -31,60 +39,60 @@ function cashArray(money) {
     ['ONE HUNDRED', 0],
   ];
 
+  // Use JSON.parse(JSON.stringify()) to deeply copy the multidimensional array.
+  let cid = JSON.parse(JSON.stringify(cashregister));
+
+  // This is basically copied from my Roman numerals project in FCC.
   while (money > 0) {
-    if (money / 100 >= 1) {
+    if (money / 100 >= 1 && cid[8][1] > 0) {
       array[8][1] += 100;
+      cid[8][1] -= 100;
       money -= 100;
       continue;
-    } else if (money / 20 >= 1) {
+    } else if (money / 20 >= 1 && cid[7][1] > 0) {
       array[7][1] += 20;
+      cid[7][1] -= 20;
       money -= 20;
       continue;
-    } else if (money / 10 >= 1) {
+    } else if (money / 10 >= 1 && cid[6][1] > 0) {
       array[6][1] += 10;
+      cid[6][1] -= 10;
       money -= 10;
       continue;
-    } else if (money / 5 >= 1) {
+    } else if (money / 5 >= 1 && cid[5][1] > 0) {
       array[5][1] += 5;
+      cid[6][1] -= 5;
       money -= 5;
       continue;
-    } else if (money / 1 >= 1) {
+    } else if (money / 1 >= 1 && cid[4][1] > 0) {
       array[4][1] += 1;
+      cid[4][1] -= 1;
       money -= 1;
       continue;
-    } else if (money / 0.25 >= 1) {
+    } else if (money / 0.25 >= 1 && cid[3][1] > 0) {
       array[3][1] += 0.25;
+      cid[3][1] -= 0.25;
       money -= 0.25;
       continue;
-    } else if (money / 0.1 >= 1) {
+    } else if (money / 0.1 >= 1 && cid[2][1] > 0) {
       array[2][1] += 0.1;
+      cid[2][1] -= 0.1;
       money -= 0.1;
       continue;
-    } else if (money / 0.05 >= 1) {
+    } else if (money / 0.05 >= 1 && cid[1][1] > 0) {
       array[1][1] += 0.05;
+      cid[1][1] -= 0.05;
       money -= 0.05;
       continue;
-    } else {
-      array[0][1] += Math.round(money * 100) / 100;
+    } else if (cid[0][1] >= money) {
+      array[0][1] += Math.ceil(money * 100) / 100;
+      cid[0][1] = 0;
       money = 0;
+      continue;
+    } else {
+      // Return false in case that there is not enough cash in the register.
+      return false;
     }
   }
-
-  const result = array.filter((item) => item[1] > 0);
-
-  return result;
+  return array;
 }
-
-let hey = checkCashRegister(3.26, 100, [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100],
-]);
-
-console.log(hey);
